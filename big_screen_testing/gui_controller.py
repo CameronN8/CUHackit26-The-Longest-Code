3c7a75e03b291cd4a6b7856e7c9c5fc1922ff213
cgouin2004 <cgouin@clemson.edu>
2026-02-28 02:51:07 -0500

    testing big screen

--- big_screen_testing/gui_controller.py
+++ big_screen_testing/gui_controller.py
@@ -156,8 +156,8 @@ class BigScreenViewer:
 
         for col in range(3):
             grid.grid_columnconfigure(col, weight=1, uniform="col")
-        grid.grid_rowconfigure(0, weight=13)
-        grid.grid_rowconfigure(1, weight=7)
+        grid.grid_rowconfigure(0, weight=2)
+        grid.grid_rowconfigure(1, weight=1)
 
         self.panel_top_left = self._make_panel(grid, "Resources Gained")
         self.panel_top_left.grid(row=0, column=0, sticky="nsew", padx=6, pady=6)
