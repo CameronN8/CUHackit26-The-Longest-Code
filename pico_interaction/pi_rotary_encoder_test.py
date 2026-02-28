da934a9fadcedc1245c0451b7fc8137fc04d6aac
cgouin2004 <cgouin@clemson.edu>
2026-02-28 12:41:02 -0500

    encoder test

--- pico_interaction/pi_rotary_encoder_test.py
+++ pico_interaction/pi_rotary_encoder_test.py
@@ -43,9 +43,9 @@ def main():
                 else:
                     count -= 1
                 print("count =", count)
-                print(GPIO.input(ENC_DT_PIN))
+                # print(GPIO.input(ENC_DT_PIN))
                 last_clk = clk_now
-            print(GPIO.input(ENC_DT_PIN), GPIO.input(ENC_CLK_PIN))
+            # print(GPIO.input(ENC_DT_PIN), GPIO.input(ENC_CLK_PIN))
 
             btn_now = GPIO.input(ENC_SW_PIN)
             now = time.monotonic()
